6611de96e2c788971e8ce3e54dd7e53573436fc7
Sławomir Albiński <slawomiralbinski@gmail.com>
2023-04-12 21:45:57 +0200

    a

---
+++ src/components/SearchMovies/index.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import { Link, useLocation } from 'react-router-dom';
+import styles from './SearchMovies.module.css';
+
+const SearchMovies = ({ movies }) => {
+  const location = useLocation();
+
+  if (!movies) return;
+
+  return (
+    <ul>
+      {movies.map(movie => {
+        return (
+          <li className={styles.item} key={movie.id}>
+            <Link
+              to={`${movie.id}`}
+              state={{ from: location }}
+              className={styles.title}
+            >
+              {movie.title}
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+SearchMovies.propTypes = {
+  movies: PropTypes.array.isRequired,
+};
+
+export default SearchMovies;
---
+++ src/utils/hooks/useCast.jsx
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { fetchCredits } from '../api';
+
+export const useCast = movieId => {
+  const [cast, setCast] = useState(null);
+
+  useEffect(() => {
+    if (movieId)
+      fetchCredits(movieId).then(res => {
+        setCast([...res]);
+      });
+  }, [movieId]);
+
+  return { cast };
+};
