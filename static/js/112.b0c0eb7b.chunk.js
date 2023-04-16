0da31b6c34fc4bca34f2c8518325b3ebf30e514b
Drwalbin <99756243+Drwalbin@users.noreply.github.com>
2023-04-16 09:50:47 +0000

    Deploying to gh-pages from @ Drwalbin/goit-react-hw-05-movies@3eb68d0b33e93ef984621a2858175bb9019b6873 🚀

---
+++ static/js/112.b0c0eb7b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[112],{1112:function(e,t,i){i.r(t),i.d(t,{default:function(){return o}});var r=i(7689),s=i(1087),c="SearchMovies_item__oZwux",n="SearchMovies_title__9ws98",a=i(184),o=function(e){var t=e.movies,i=(0,r.TH)();if(t)return(0,a.jsx)("ul",{children:t.map((function(e){return(0,a.jsx)("li",{className:c,children:(0,a.jsx)(s.rU,{to:"".concat(e.id),state:{from:i},className:n,children:e.title})},e.id)}))})}}}]);
+//# sourceMappingURL=112.b0c0eb7b.chunk.js.map
